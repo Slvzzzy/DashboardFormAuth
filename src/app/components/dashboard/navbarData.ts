d353251c2539468cdd9a1d1c219c066c91c9905c
Slvzzzy <dexiar77@gmail.com>
2023-10-23 23:42:07 +0700

    created the Login, Registration, Verification, Forgot, and dashboard pages. Added a routing module, an authentication service and a guard system. Made a connection to the Firebase database

---
+++ src/app/components/dashboard/navbarData.ts
@@ -0,0 +1,43 @@
+export const navBarDate = [
+  { title: "Menu", link: [{
+      label: "Dashboard",
+    },
+      {
+        label: "Message",
+      },
+      {
+        label: "Calendar",
+      }]},
+
+  {title: "Organization", link: [{
+      label: "Employee",
+    },
+      {
+        label: "Structure",
+        icon: "view_list",
+      },
+      {
+        label: "Report",
+        icon: "report",
+      },
+      {
+        label: "Settings",
+        icon: "settings",
+      }]},
+  {title: "Additionally", link: [{
+      label: "Main office",
+      icon: "map",
+    },
+      {
+        label: "Training program",
+        icon: "accessibility",
+      },
+      {
+        label: "Job openings",
+        icon: "account_box",
+      },
+      {
+        label: "Rules",
+        icon: "check_circle",
+      }]}
+]
---
+++ src/environments/enviorment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBhNwJAtZ8mlnSSWgQPIozvq8qdsSpDMxc",
+    authDomain: "dashboardformaauth.firebaseapp.com",
+    projectId: "dashboardformaauth",
+    storageBucket: "dashboardformaauth.appspot.com",
+    messagingSenderId: "1033585898651",
+    appId: "1:1033585898651:web:51d2d66046de4d2b35023e",
+    measurementId: "G-205N905YKK"
+  }
+};
